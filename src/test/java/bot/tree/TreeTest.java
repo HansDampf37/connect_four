8ca4e789caab7a96c08c0e5f072ecbc27a6cd02b
Adrian Degenkolb <adrian@degenkolb.de>
2020-07-30 16:21:53 +0200

    implemented Iterable<Node> to Tree and created unit tests Tree

---
+++ src/test/java/bot/tree/TreeTest.java
@@ -0,0 +1,13 @@
+package bot.tree;
+
+import org.junit.Before;
+
+public class NodeTest {
+    private Node node;
+    private Tree tree;
+
+    @Before
+    public void setup() {
+        tree = new Tree(3, 3);
+    }
+}
