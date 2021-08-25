65ff4382b4f46339d89ad4718bcd91eff22c8b0c
adrian <adrian@degenkolb.de>
2021-08-25 23:12:44 +0200

    fix bugs and introduce multithreading to Alphabetapruning

--- src/main/java/bot/bots/tree/AlphaBetaPruning.kt
+++ src/main/java/bot/bots/tree/AlphaBetaPruning.kt
@@ -1,27 +1,58 @@
 package bot.bots.tree
 
+import kotlinx.coroutines.*
 import java.lang.Integer.min
 import kotlin.math.max
 
 class AlphaBetaPruning {
     companion object {
         fun <T : GameState> run(t: Tree<T>): Int {
-            return step(t.root, Integer.MIN_VALUE, Integer.MAX_VALUE, true)
+            return runBlocking {
+                step(
+                    t.root, Integer.MIN_VALUE, Integer.MAX_VALUE,
+                    maximisingPlayer = true,
+                    firstLevel = true
+                )
+            }
         }
 
-        private fun <T : GameState> step(node: T, alpha: Int, beta: Int, maximisingPlayer: Boolean): Int {
+        private suspend fun <T : GameState> step(
+            node: T,
+            alpha: Int,
+            beta: Int,
+            maximisingPlayer: Boolean,
+            firstLevel: Boolean
+        ): Int {
             var alpha1 = alpha
             var beta1 = beta
             if (node.isLeaf) return 0
             if (maximisingPlayer) {
                 var maxColumn = -1
                 var maxValue = Integer.MIN_VALUE
-                for (i in node.indices) {
-                    step(node[i], alpha1, beta1, false)
-                    val value = node[i].value
-                    if (value > maxValue) {
+                if (firstLevel) {
+                    val futures = ArrayList<Deferred<Int>>()
+                    for (child in node) {
+                        if (firstLevel) futures.add(GlobalScope.async {
+                            step(
+                                child,
+                                alpha1,
+                                beta1,
+                                false,
+                                firstLevel = false
+                            )
+                        })
+                    }
+                    futures.forEach { it.await() }
+                } else {
+                    for (child in node) {
+                        step(child, alpha1, beta1, false, firstLevel = false)
+                    }
+                }
+                for (child in node) {
+                    val value = child.value
+                    if (value >= maxValue) {
                         maxValue = value
-                        maxColumn = node[i].lastMoveWasColumn
+                        maxColumn = child.lastMoveWasColumn
                     }
                     alpha1 = max(alpha1, maxValue)
                     if (beta1 <= alpha1) break
@@ -31,12 +62,30 @@ class AlphaBetaPruning {
             } else {
                 var minColumn = -1
                 var minValue = Integer.MAX_VALUE
-                for (i in node.indices) {
-                    step(node[i], alpha1, beta1, true)
-                    val value = node[i].value
-                    if (value < minValue) {
+                if (firstLevel) {
+                    val futures = ArrayList<Deferred<Int>>()
+                    for (child in node) {
+                        if (firstLevel) futures.add(GlobalScope.async {
+                            step(
+                                child,
+                                alpha1,
+                                beta1,
+                                true,
+                                firstLevel = false
+                            )
+                        })
+                    }
+                    futures.forEach { it.await() }
+                } else {
+                    for (child in node) {
+                        step(child, alpha1, beta1, true, firstLevel = false)
+                    }
+                }
+                for (child in node) {
+                    val value = child.value
+                    if (value <= minValue) {
                         minValue = value
-                        minColumn = node[i].lastMoveWasColumn
+                        minColumn = child.lastMoveWasColumn
                     }
                     beta1 = min(beta1, value)
                     if (beta1 <= alpha1) break
